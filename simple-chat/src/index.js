61deabc5599ae8bcbf3b56d5ee09dae1876cc2fa
hermanfrelikh <scaredboy134@gmail.com>
2024-10-14 15:50:34 +0300

    Fix errors

--- simple-chat/src/index.js
+++ simple-chat/src/index.js
@@ -19,7 +19,7 @@ if (users.length === 0) {
       avatar:
         "https://sun9-44.userapi.com/impg/Fmp3R9zTLJnkrYrJgRPPq8IfQ2tClWKByoHv9w/Lj9oGtVjToE.jpg?size=1080x1440&quality=95&sign=7019cbcdb9a7e615206611f82c34838a&type=album",
       status: "недавно",
-      lastMessage: "последнее сообщение не очень длинное",
+      lastMessage: "Нет сообщений",
     },
     {
       id: 2,
@@ -27,7 +27,7 @@ if (users.length === 0) {
       avatar:
         "https://sun9-76.userapi.com/impg/5v6W30Tm2lroaFXtPZPwO5OlktQMJVyCcElQoQ/7tijVbcubrA.jpg?size=828x828&quality=95&sign=9efdebd108a93b8e9653280f7fdc7c83&type=album",
       status: "онлайн",
-      lastMessage: "последнее сообщение не очень длинное",
+      lastMessage: "Нет сообщений",
     },
     {
       id: 3,
@@ -35,7 +35,7 @@ if (users.length === 0) {
       avatar:
         "https://sun1-27.userapi.com/impg/1_NloAcb3e9eHWuE40CZDT2oBtnwvXBLTzu2vA/kqmti69382o.jpg?size=1080x1399&quality=95&sign=49d0ef831fe99ae2418bcd8b82811342&type=album",
       status: "2 часа назад",
-      lastMessage: "последнее сообщение не очень длинное",
+      lastMessage: "Нет сообщений",
     },
   ];
   saveUsers(users);
@@ -43,8 +43,7 @@ if (users.length === 0) {
 
 export { users, getUsers, saveUsers };
 
-let currentUser = null;
-let currentSenderId = null;
+let contact = null;
 
 document.addEventListener("DOMContentLoaded", () => {
   const headerContainersIndex = document.getElementById("header-container-index");
@@ -61,71 +60,45 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector(".form-input");
   const messagesContainer = document.querySelector(".messages");
   const sendButton = document.querySelector(".sendButton");
-  const syncIcon = document.querySelector(".icon-button");
-  const personName = document.querySelector(".personName");
-  const personPhoto = document.querySelector(".personPhoto");
-  const personStatus = document.querySelector(".personStatus");
+  const switchAccountButton = document.querySelector(".icon-button");
+  const contactName = document.querySelector(".personName");
+  const contactAvatar = document.querySelector(".personPhoto");
+  const contactStatus = document.querySelector(".personStatus");
 
-  const german = {
+  const mainAccount = {
     id: "german",
     name: "Герман",
     avatar:
       "https://sun1-24.userapi.com/impg/7rPDGVoAQDvDZ55XLQ8fvqPoXagiitcUTDu4Hg/MiqLb9edxMU.jpg?size=2560x2560&quality=95&sign=b7abba0e9f3dadc571504bdff665cf4f&type=album",
     status: "онлайн",
-    lastMessage: "последнее сообщение не очень длинное",
+    lastMessage: "Нет сообщений",
   };
 
   const urlParams = new URLSearchParams(window.location.search);
-  let currentUserIndex = parseInt(urlParams.get("chatId")) - 1;
-  currentUser = users[currentUserIndex];
-  let currentUserOther = german;
+  let chatId = parseInt(urlParams.get("chatId")) - 1;
+  contact = users[chatId];
+  let account = mainAccount;
 
-  if (currentUser) {
-    personName.textContent = currentUser.name;
-    personPhoto.src = currentUser.avatar;
-    personStatus.textContent = currentUser.status;
+  const renderContact = (contact) => {
+    if (!contact) {
+      return;
+    }
+    contactName.textContent = contact.name;
+    contactAvatar.src = contact.avatar;
+    contactStatus.textContent = contact.status;
   }
 
-  if (syncIcon !== null) {
-    syncIcon.addEventListener("click", switchUser);
+  renderContact(contact);
+
+  if (switchAccountButton !== null) {
+    switchAccountButton.addEventListener("click", switchUser);
   }
 
   function switchUser() {
-    if (currentUser.id === german.id) {
-      currentUserOther = currentUser;
-      currentUser = users[currentUserIndex];
-    } else {
-      currentUserOther = currentUser;
-      currentUser = german;
-    }
+    account = contact;
+    contact = (contact === mainAccount) ? users[chatId] : mainAccount;
 
-    personName.textContent = currentUser.name;
-    personPhoto.src = currentUser.avatar;
-    personStatus.textContent = currentUser.status;
-    updateMessages();
-  }
-
-  function updateMessages() {
-    const messages1 =
-      JSON.parse(localStorage.getItem(`messages_${currentUserOther.id}`)) || [];
-    for (let i = 0; i < messages1.length; i++) {
-      messages1[i].self = !messages1[i].self;
-    }
-    localStorage.setItem(
-      `messages_${currentUserOther.id}`,
-      JSON.stringify(messages1)
-    );
-    const messages2 =
-      JSON.parse(
-        localStorage.getItem(`messages_german_${currentUserOther.id}`)
-      ) || [];
-    for (let i = 0; i < messages2.length; i++) {
-      messages2[i].self = !messages2[i].self;
-    }
-    localStorage.setItem(
-      `messages_german_${currentUserOther.id}`,
-      JSON.stringify(messages2)
-    );
+    renderContact(contact);
     loadMessages();
   }
 
@@ -147,13 +120,21 @@ document.addEventListener("DOMContentLoaded", () => {
         time: new Date()
           .toLocaleTimeString()
           .slice(0, new Date().toLocaleTimeString().length - 3),
-        self: true,
-        senderId: currentUser.id,
+        isMainAccountSender: account === mainAccount,
       };
       saveMessage(message);
       displayMessage(message);
       input.value = "";
       scrollToBottom();
+
+      if (messageText.length>50){
+        contact.lastMessage = messageText.slice(0,50)+"..."
+      }
+      else{
+        contact.lastMessage = messageText;
+      }
+      
+      saveUsers(users);
     }
   }
 
@@ -164,39 +145,29 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function saveMessage(message) {
-    if (currentUser === german) {
-      let messages =
-        JSON.parse(
-          localStorage.getItem(`messages_german_${currentUserOther.id}`)
-        ) || [];
-      messages.push(message);
-      localStorage.setItem(
-        `messages_german_${currentUserOther.id}`,
-        JSON.stringify(messages)
-      );
-    } else {
-      let messages =
-        JSON.parse(localStorage.getItem(`messages_${currentUser.id}`)) || [];
-      messages.push(message);
-      localStorage.setItem(
-        `messages_${currentUser.id}`,
-        JSON.stringify(messages)
-      );
-    }
+    let messages = JSON.parse(localStorage.getItem(`messages_${chatId}`)) || [];
+    messages.push(message);
+    localStorage.setItem(
+      `messages_${chatId}`,
+      JSON.stringify(messages)
+    );
   }
 
   function displayMessage(message) {
     const messageElement = document.createElement("div");
     messageElement.classList.add(
       "message",
-      message.self ? "message-self" : "message-other"
+      (message.isMainAccountSender && (account === mainAccount) || !message.isMainAccountSender && (account !== mainAccount)) ? "message-self" : "message-other"
     );
     messageElement.setAttribute("data-id", message.id);
-    messageElement.setAttribute("data-self", message.self);
-    messageElement.setAttribute("data-user-id", currentUser.id);
+    messageElement.setAttribute("data-self", (message.isMainAccountSender && (account === mainAccount) || !message.isMainAccountSender && (account !== mainAccount)));
+    messageElement.setAttribute("data-user-id", contact.id);
     messageElement.innerHTML = `
-      <p class="message-text">${message.text}</p>
-      <span id="done-all-chat" class="material-symbols-outlined icon">done_all</span>
+      <div class="message-text-done-all-chat">
+        <p class="message-text">${message.text}</p>
+        <span id="done-all-chat" class="material-symbols-outlined icon">done_all</span>
+      </div>
+      
       <p class="message-time">${message.time}</p>
       
     `;
@@ -204,19 +175,13 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function loadMessages() {
-    if (currentUser) {
-      const messages =
-        JSON.parse(localStorage.getItem(`messages_${currentUser.id}`)) || [];
+    if (messagesContainer){
+      messagesContainer.innerHTML = '';
+    }
+    
+    if (contact) {
+      const messages = JSON.parse(localStorage.getItem(`messages_${chatId}`)) || [];
       messages.forEach((message) => {
-        if (!document.querySelector(`[data-id="${message.id}"]`)) {
-          displayMessage(message);
-        }
-      });
-      const messagesOther =
-        JSON.parse(localStorage.getItem(`messages_german_${currentUser.id}`)) ||
-        [];
-
-      messagesOther.forEach((message) => {
         if (!document.querySelector(`[data-id="${message.id}"]`)) {
           displayMessage(message);
         }
@@ -230,4 +195,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   loadMessages();
+
+  
 });
+
